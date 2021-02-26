652051458d1979e81c597b0689f1cb9735018388
Homayoun Ostad <hhhomayoun@yahoo.com>
2021-02-26 10:49:34 -0800

    props basics

--- src/index.js
+++ src/index.js
@@ -2,37 +2,46 @@ import React from 'react'
 import ReactDom from 'react-dom'
 // css
 import './index.css'
+
+// vars
+
+const firstBook = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/51PRQuO-xjL._AC_SX184_.jpg',
+  title: 'this is your favorite book',
+  author: 'homayoun ostad',
+}
+const secondBook = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._AC_SX184_.jpg',
+  title: 'this is your second favorite book',
+  author: 'homayoun ostad',
+}
 function Greeting() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
-    <article>
-      <img
-        src='https://images-na.ssl-images-amazon.com/images/I/51PRQuO-xjL._AC_SX184_.jpg'
-        alt=''
-      />
-      <Title />
-      <Author />
+    <article className='book'>
+      <img src={props.img} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   )
 }
-const Title = () => {
-  return <p>this is your book</p>
-}
-const Author = () => {
-  return <h6>homayoun ostad</h6>
-}
+
 ReactDom.render(<Greeting />, document.getElementById('root'))
